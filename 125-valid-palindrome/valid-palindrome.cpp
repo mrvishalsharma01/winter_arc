824ebe096437b12be858d71b2272ea385b07a185
Vishal Sharma <outreach.vishal@gmail.com>
2026-02-20 11:10:05 +0530

    Time: 3 ms (30.99%) | Memory: 9.9 MB (53.73%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string cleaned="";
-        for(char c:s){
-            if(isalnum(c)){
-                cleaned+=tolower(c);
+        int left=0; 
+        int right=s.length()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])){
+                left++;
             }
+            while(left < right && !isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
         }
-        string rev=cleaned;
-        reverse(rev.begin(),rev.end());
-        return cleaned==rev;
+        return true;
     }
 };
